294386759433556e9bc3aa95fd17d3ad1a16920a
eod940 <eod940@gmail.com>
2022-09-07 23:43:02 +0900

    (feature/diary/posts-api) test_password_validate 작성

--- diary_service/urls.py
+++ diary_service/urls.py
@@ -18,8 +18,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/diary/', include('diary.urls')),
 ]
 
 if settings.DEBUG:
